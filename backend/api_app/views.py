40420b0c39f82e5f2c131e2029b35136f84e3af6
Poleron402 <zhperon@gmail.com>
2024-01-03 23:00:12 -0800

    troubleshooting

--- backend/api_app/views.py
+++ backend/api_app/views.py
@@ -5,7 +5,8 @@ from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 import requests
 from openai import OpenAI
 from cryptoproj.settings import env
-client = OpenAI(api_key=env.get("OPENAI_API_KEY"))
+OPENAI_API_KEY = env.get("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
 # Create your views here.
 class PromptView(APIView):
     def get(self, request, crypto):
