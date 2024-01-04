88f9e27caa48650e85898795fc99a173095582f5
Poleron402 <zhperon@gmail.com>
2024-01-04 12:26:31 -0800

    fxing small things

--- backend/cryptoproj/settings.py
+++ backend/cryptoproj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # This used to be empty
 ALLOWED_HOSTS = ["*"]
 # This used to not be here
