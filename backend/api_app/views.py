633ee77a4bfa8fea51a4cb500e2c60fb911d3014
Poleron402 <zhperon@gmail.com>
2024-01-03 23:15:20 -0800

    troubleshooting

--- backend/api_app/views.py
+++ backend/api_app/views.py
@@ -8,6 +8,8 @@ from cryptoproj.settings import env
 from dotenv import load_dotenv
 
 import os
+load_dotenv()
+
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 # Create your views here.
